d0da2c39b467f440c96a61df1c4539011dc8f56e
Pavithra K <pksjce@gmail.com>
2016-10-30 11:51:02 +0530

    Allows selecting property to edit

--- components/allOf.js
+++ components/allOf.js
@@ -4,7 +4,7 @@ class AllOf extends Component {
     render() {
         return <div data-type='allOf'>
             {this.props.children.map((child, index) => {
-                return <div>
+                return <div key={index}>
                     {child}
                     {-1 < index  && index < (this.props.children.length - 1) ? <div> And </div>: null}
                 </div>
--- components/index.js
+++ components/index.js
@@ -3,27 +3,32 @@ import {connect} from 'react-redux';
 import makeUI from '../utils';
 
 export class RenderSchema extends Component{
-    render() {
-        function handleClick(property) {
-            dispatch({
-                type: 'CHOOSE_PROP',
-                property
-            })
+    constructor() {
+        super();
+        this.state = {
+            selectedProp: 'entry'
         }
+    }
+    handleClick(prop){
+        this.setState({
+            selectedProp: prop
+        })
+    }
+    render() {
         return <div>
             <h1>Cook your own webpack</h1>
             <div class='container'>
                 <div class='left'>
                     <ul>
                     {['entry', 'externals', 'context', 'dependencies', 'devtool', 'name'].map((property, id) => 
-                        <li key={id}>{property}</li>
+                        <li key={id} onClick={this.handleClick.bind(this, property)}>{property}</li>
                     )}
                     </ul>
                 </div>
                 <div class='right'>
                     <div class='next-node'>
-                        <div>{'entry'.toUpperCase()}</div>
-                        <div >{makeUI('entry').elementUI}</div>
+                        <div>{this.state.selectedProp.toUpperCase()}</div>
+                        <div >{makeUI(this.state.selectedProp).elementUI}</div>
                     </div>
                 </div>
             </div>
